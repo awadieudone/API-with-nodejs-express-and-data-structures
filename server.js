e63e97ed2bc28f986b3e2d88415c5c0b5118d0dc
awadieudone <awadieudone@gmail.com>
2019-04-20 14:54:05 +0100

    Set up model and routes of the application

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import Reflection from './src/controllers/Reflection';
 
 const app = express();
+
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections/:id', Reflection.delete);
 
 app.use(express.json());
 
