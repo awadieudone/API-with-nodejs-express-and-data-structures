f4eb3d4616fc70dc0f16c5677ddba37decc3bafd
awadieudone <awadieudone@gmail.com>
2019-04-28 08:58:05 +0100

    Build Endpoints for the application

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-import express from 'express';
-import Reflection from './src/controllers/Reflection';
+import express from "express";
+import Reflection from "./src/controllers/Reflection";
+import bodyParser from "body-parser";
+import logger from 'morgan';
+import path from 'path';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.post('/api/v1/reflections', Reflection.create);
-app.get('/api/v1/reflections', Reflection.getAll);
-app.get('/api/v1/reflections/:id', Reflection.getOne);
-app.put('/api/v1/reflections/:id', Reflection.update);
-app.delete('/api/v1/reflections/:id', Reflection.delete);
-
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
 
-app.get('/', (req, res) => {
-    return res.status(200).send({ 'message': 'YAY! Congratulations! Your first endpoint is working' });
+app.post("/api/v1/reflections", Reflection.postOne);
+app.get("/api/v1/reflections", Reflection.getAll);
+app.get("/api/v1/reflections/:id", Reflection.getOne);
+app.put("/api/v1/reflections/:id", Reflection.update);
+app.delete("/api/v1/reflections/:id", Reflection.delete);
+
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .send({ message: "YAY! Congratulations! Your first endpoint is working" });
 });
 
 app.listen(3000);
-console.log('app running on port ', 3000);
+console.log("app running on port ", 3000);
--- src/controllers/Reflection.js
+++ src/controllers/Reflection.js
@@ -1,70 +1,72 @@
-import ReflectionModel from '../models/Reflection';
+import ReflectionModel from "../models/Reflection";
 
 const Reflection = {
-    /**
-     * 
-     * @param {object} req 
-     * @param {object} res
-     * @returns {object} reflection object 
-     */
-    create(req, res) {
-        if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
-            return res.status(400).send({ 'message': 'All fields are required' })
-        }
-        const reflection = ReflectionModel.create(req.body);
-        return res.status(201).send(reflection);
-    },
-    /**
-     * 
-     * @param {object} req 
-     * @param {object} res 
-     * @returns {object} reflections array
-     */
-    getAll(req, res) {
-        const reflections = ReflectionModel.findAll();
-        return res.status(200).send(reflections);
-    },
-    /**
-     * 
-     * @param {object} req 
-     * @param {object} res
-     * @returns {object} reflection object
-     */
-    getOne(req, res) {
-        const reflection = ReflectionModel.findOne(req.params.id);
-        if (!reflection) {
-            return res.status(404).send({ 'message': 'reflection not found' });
-        }
-        return res.status(200).send(reflection);
-    },
-    /**
-     * 
-     * @param {object} req 
-     * @param {object} res 
-     * @returns {object} updated reflection
-     */
-    update(req, res) {
-        const reflection = ReflectionModel.findOne(req.params.id);
-        if (!reflection) {
-            return res.status(404).send({ 'message': 'reflection not found' });
-        }
-        const updatedReflection = ReflectionModel.update(req.params.id, req.body)
-        return res.status(200).send(updatedReflection);
-    },
-    /**
-     * 
-     * @param {object} req 
-     * @param {object} res 
-     * @returns {void} return statuc code 204 
-     */
-    delete(req, res) {
-        const reflection = ReflectionModel.findOne(req.params.id);
-        if (!reflection) {
-            return res.status(404).send({ 'message': 'reflection not found' });
-        }
-        const ref = ReflectionModel.delete(req.params.id);
-        return res.status(204).send(ref);
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} reflection object
+   */
+  postOne(req, res) {
+    // if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
+    // return res.status(400).json({ message: "All fields are required" });
+    // }
+
+    console.log(req.body);
+    const reflection = ReflectionModel.create(req.body);
+    return res.status(201).json(reflection);
+  },
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} reflections array
+   */
+  getAll(req, res) {
+    const reflections = ReflectionModel.findAll();
+    return res.status(200).json(reflections);
+  },
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} reflection object
+   */
+  getOne(req, res) {
+    const reflection = ReflectionModel.findOne(req.params.id);
+    if (!reflection) {
+      return res.status(404).json({ message: "reflection not found" });
     }
-}
+    return res.status(200).json(reflection);
+  },
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} updated reflection
+   */
+  update(req, res) {
+    const reflection = ReflectionModel.findOne(req.params.id);
+    if (!reflection) {
+      return res.status(404).json({ message: "reflection not found" });
+    }
+    const updatedReflection = ReflectionModel.update(req.params.id, req.body);
+    return res.status(200).json(updatedReflection);
+  },
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {void} return statuc code 204
+   */
+  delete(req, res) {
+    const reflection = ReflectionModel.findOne(req.params.id);
+    if (!reflection) {
+      return res.status(404).json({ message: "reflection not found" });
+    }
+    const ref = ReflectionModel.delete(req.params.id);
+    return res.status(204).json(ref);
+  }
+};
 
 export default Reflection;
